66a8f0cdda43e9a68d3625c56396df02589c24e3
Ítalo Lima <italojonas@hotmail.com>
2024-09-28 19:17:37 -0300

    feat: add route to campaign

--- src/app/_components/campaign/index.tsx
+++ src/app/_components/campaign/index.tsx
@@ -1,5 +1,6 @@
 import { useCampaign } from '@/app/hooks/use-campaign';
 import { Button } from '../ui/button';
+import Link from 'next/link';
 
 export const Campaign = () => {
   const { campaign } = useCampaign();
@@ -8,8 +9,11 @@ export const Campaign = () => {
     <div className="bg-primary px-4 py-10 text-gray-50 text-center">
       <h1 className="text-3xl font-semibold pb-4">{campaign?.name}</h1>
       <p>{campaign?.description}</p>
-      <Button className="bg-primary text-gray-50 border-gray-50 border rounded-full mt-4">
-        Participar agora
+      <Button
+        className="bg-primary text-gray-50 border-gray-50 border rounded-full mt-4"
+        asChild
+      >
+        <Link href="/campaign">Participar agora</Link>
       </Button>
     </div>
   );
